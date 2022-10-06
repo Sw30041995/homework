23babd9bb3999cca0b131b6d12e25d576b1240c9
sw30041995 <sw1225@yandex.ru>
2022-10-06 19:45:00 +0300

    dropdown menu added

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './Header.module.css'
 
 function Header() {
+
+    const [focus, setFocus] = useState<boolean>(false)
+
+    const removeFocus = () => {
+        setFocus(false)
+    }
+
     return (
-        <div>
-            <div><NavLink to={'/pre-junior'}>PreJunior</NavLink></div>
-            <div><NavLink to={'/junior'}>Junior</NavLink></div>
-            <div><NavLink to={''}>Junior+</NavLink></div>
+        <div className={s.header}>
+            <div onMouseOver={() => setFocus(true)} className={s.menu}>Меню</div>
+            {focus &&
+            <div className={s.link}>
+                <div onClick={removeFocus} className={s.item}><NavLink to={PATH.PRE_JUNIOR}>PreJunior</NavLink></div>
+                <div onClick={removeFocus} className={s.item}><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
+                <div onClick={removeFocus} className={s.item}><NavLink to={''}>Junior+</NavLink></div>
+            </div>}
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.errorBlock}>
+            <div className={s.error}>Page not found!</div>
+            <div>
+                <img className={s.img} src="https://img.freepik.com/free-vector/oops-404-error-with-a-broken-robot-concept-illustration_114360-5529.jpg?w=2000g" alt="Error 404"/>
+            </div>
         </div>
     )
 }
