67e7836547e062dfeaf60c45d0b4c3de63b0155f
sw30041995 <sw1225@yandex.ru>
2022-10-12 18:35:11 +0300

    homework 9 and 10 is completed

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {loadingAC} from "./bll/loadingReducer";
+import {AppStoreType} from "./bll/store";
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+
+    const dispatch = useDispatch()
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.loading)
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
+        dispatch(loadingAC(true))
+        setInterval(() => {
+            dispatch(loadingAC(false))
+        }, 2000)
     };
 
     return (
@@ -17,10 +22,10 @@ function HW10() {
             homeworks 10
 
             {/*should work (должно работать)*/}
-            {loading
-                ? (
-                    <div>крутилка...</div>
-                ) : (
+            {loading ?
+                <div><img style={{width: '100px', height: '100px'}}
+                          src="https://flevix.com/wp-content/uploads/2019/07/Bubble-Preloader-1-1.gif" alt="loading"/>
+                </div> : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,28 @@
-const initState = {
+export type initStateType = {
+    loading: boolean
+}
+type loadingACType = ReturnType<typeof loadingAC>
+type ActionType = loadingACType
 
+const initState: initStateType = {
+    loading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): initStateType => {
     switch (action.type) {
-        case '': {
-            return state
+        case 'LOADING': {
+            return {...state, loading: action.payload.loading}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (value: boolean) => {
+    return {
+        type: 'LOADING',
+        payload: {
+            loading: value
+        }
+    } as const
+}
